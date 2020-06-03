588560dcc557ac4591d51a867b194781cc75cc86
陈欢斌 <1084271324@qq.com>
2020-06-03 08:48:10 +0800

    优化

--- webpack.config.bundle.js
+++ webpack.config.bundle.js
@@ -58,7 +58,7 @@ module.exports = {
     module: {
         rules: [
             { 
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {  
                     loader: 'babel-loader'
