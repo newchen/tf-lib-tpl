b1c086320ad05acf9be3ed40650a61ba3818e65c
陈欢斌 <1084271324@qq.com>
2021-02-25 11:17:51 +0800

    更新模板

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,33 +1,17 @@
-import React, { Component } from 'react'
+import React, { FC } from 'react'
 import styles from './index.less';
 
 interface IProps {
   // todo
+
 }
 
-interface IState {
-  // todo
+export const TfXXX: FC<IProps> = () => {
+  return(
+    <div className={styles.test}>
+      xxxxxx
+    </div>
+  )
 }
 
-export default class TfXXX extends Component<IProps, IState> {
-  static defaultProps = {
-    
-  }
-  state = {
-    
-  }
-  constructor(props) {
-    super(props);
-
-  }
-  render() {
-
-    return(
-      <div className={styles.test}>
-        xxxxxx
-      </div>
-    )
-  }
-}
-
-export { TfXXX }
+export default TfXXX;
